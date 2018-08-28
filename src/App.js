86092b9eb1d51e70194c16e222672edc004c806b
saigalamatya <raj.maharjan98@gmail.com>
2018-08-28 20:03:26 +0545

    project setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Sidebar from './Sidebar';
+import Main from './Main';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
